6333a2cbfb2f4902e2919d6699634e8a90779df5
vorozhkog <vorozkhog@icloud.com>
2024-03-07 17:45:40 +0000

    preview id change

--- src/settings.py
+++ src/settings.py
@@ -39,7 +39,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://data.mendeley.com/datasets/yy2k5y8mxg/1"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 14100249
+PREVIEW_IMAGE_ID: int = 14684299
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/bracol"
